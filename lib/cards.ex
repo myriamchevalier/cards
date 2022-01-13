b4c0b84910084c81bbfa21d050ed858eeff41d33
Myriam Chevalier <mchevalier@bitfreighter.com>
2022-01-13 13:00:21 -0600

    create_dec, shuffle, and deal functions added

--- lib/cards.ex
+++ lib/cards.ex
@@ -4,7 +4,25 @@ defmodule Cards do
   end
 
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # List comprehension (for loop in Elixir)
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 end
 
